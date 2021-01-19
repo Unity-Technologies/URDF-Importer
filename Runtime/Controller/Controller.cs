94b800c4c486522ddf44e56ad3daa679389cc147
LaurieCheers <73140792+LaurieCheers-unity@users.noreply.github.com>
2021-01-19 13:30:50 -0800

    Fix merge conflict

--- Runtime/Controller/Controller.cs
+++ Runtime/Controller/Controller.cs
@@ -222,7 +222,7 @@ namespace RosSharp.Control
             GUIStyle centeredStyle = GUI.skin.GetStyle("Label");
             centeredStyle.alignment = TextAnchor.UpperCenter;
             GUI.Label(new Rect(Screen.width / 2 - 200, 10, 400, 20), "Press left/right arrow keys to select a robot joint.", centeredStyle);
-            GUI.Label(new Rect(Screen.width / 2 - 200, 30, 400, 20), "Press up/down arrow keys to move " + jointName + ".", centeredStyle);
+            GUI.Label(new Rect(Screen.width / 2 - 200, 30, 400, 20), "Press up/down arrow keys to move " + selectedJoint + ".", centeredStyle);
         }
     }
 }
