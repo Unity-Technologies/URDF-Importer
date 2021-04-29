5ad9c201a37110dc368249b79a946b8af7c5378a
vidurvij-Unity <60901103+vidurvij-Unity@users.noreply.github.com>
2021-04-29 15:29:10 -0700

    Update names according to naming convenetion

--- com.unity.robotics.urdf-importer/Runtime/RosSharpDefinitions/Sensor.cs
+++ com.unity.robotics.urdf-importer/Runtime/RosSharpDefinitions/Sensor.cs
@@ -12,37 +12,42 @@ namespace RosSharp.Urdf
 {
     public class Sensor
     {
-        public string name;
-        public string type;
-        public string topic;
-        private static string baseKey = "sensor";
-        private static string attributeDelimit = "@";
-        private static string elementDelimit = "/";
-        public int updateRate;
-        public Dictionary<string,string> elements;
+        public string name
+        { get; set; }
+        public string type
+        { get; set; }
+        public string topic
+        { get; set; }
+        private static string k_baseKey = "sensor";
+        private static string k_attributeDelimit = "@";
+        private static string k_elementDelimit = "/";
+        public int updateRate
+        { get; set; }
+        public Dictionary<string,string> elements
+        { get; set; }
 
         public Sensor(XElement node)
         {
             elements = new Dictionary<string, string>();
             foreach(XAttribute attribute in node.Attributes())
             {
-                AddAttribute(attribute, baseKey);
+                AddAttribute(attribute, k_baseKey);
             }
             foreach(XElement element in node.Elements())
             {
-                AddElement(element,baseKey);
+                AddElement(element,k_baseKey);
             }
         }
 
         public void AddAttribute(XAttribute attribute, string key)
         {
-            string currentKey = key + attributeDelimit + attribute.Name;
+            string currentKey = key + k_attributeDelimit + attribute.Name;
             elements.Add(currentKey, attribute.Value);
         }
 
         public void AddElement(XElement element, string key)
         {
-            string currentKey = key + elementDelimit + element.Name;
+            string currentKey = key + k_elementDelimit + element.Name;
             if(element.Elements().Count() == 0 && !(element.Value == ""))
             {
                 elements.Add(currentKey,element.Value);
