7b7e926488c2af38401b2d601c2574b7b3139b3c
ingram weeks <ingram.weeks@gmail.com>
2023-10-02 16:11:51 +0100

    Added updated file from asset importer

--- com.unity.robotics.urdf-importer/Runtime/UnityMeshImporter/Plugins/AssimpNet/AssimpUnity.cs
+++ com.unity.robotics.urdf-importer/Runtime/UnityMeshImporter/Plugins/AssimpNet/AssimpUnity.cs
@@ -20,11 +20,6 @@
 * THE SOFTWARE.
 */
 
-/* Note 2019
-* 
-* This file is modified by Dongho Kang to distributed as a Unity package 2019.
-*/ 
-
 using Assimp.Unmanaged;
 using System.IO;
 using UnityEngine;
@@ -35,10 +30,11 @@ namespace Assimp
     /// AssimpNet Unity integration. This handles one-time initialization (before scene load) of the AssimpLibrary instance, setting DLL probing paths to load the correct native
     /// dependencies, if the current platform is supported.
     /// </summary>
+#if UNITY_EDITOR
+    [UnityEditor.InitializeOnLoad]
+#endif
     public class AssimpUnity
     {
-        private const string packageName = "com.unity.robotics.urdf-importer";
-        
         private static bool s_triedLoading = false;
         private static bool s_assimpAvailable = false;
 
@@ -53,17 +49,22 @@ namespace Assimp
             }
         }
 
+        static AssimpUnity()
+        {
+            InitializePlugin();
+        }
+
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         private static void InitializePlugin()
         {
             //Only try once during runtime
-            if (s_triedLoading)
+            if(s_triedLoading)
                 return;
 
             UnmanagedLibrary libInstance = AssimpLibrary.Instance;
 
             //If already initialized, set flags and return
-            if (libInstance.IsLibraryLoaded)
+            if(libInstance.IsLibraryLoaded)
             {
                 s_assimpAvailable = true;
                 s_triedLoading = true;
@@ -72,7 +73,17 @@ namespace Assimp
 
             //First time initialization, need to set a probing path (at least in editor) to resolve the native dependencies
             string pluginsFolder = Path.Combine(Application.dataPath, "Plugins");
-            string editorPluginNativeFolder = Path.Combine(Path.GetFullPath(string.Format($"Packages/{packageName}")), "Runtime", "UnityMeshImporter", "Plugins", "AssimpNet", "Native");
+            string editorPluginNativeFolder = Path.Combine(pluginsFolder, "AssimpNet", "Native");
+
+            if(!Directory.Exists(editorPluginNativeFolder))
+            {
+                // Get directory that contains this file.
+                string pluginFolder = Path.GetDirectoryName(GetCallerFilePath());
+
+                pluginsFolder = Directory.GetParent(pluginFolder).FullName;
+                editorPluginNativeFolder = Path.Combine(pluginFolder, "Native");
+            }
+
             string native64LibPath = null;
             string native32LibPath = null;
 
@@ -85,51 +96,71 @@ namespace Assimp
             switch(Application.platform)
             {
                 case RuntimePlatform.WindowsEditor:
-                    native64LibPath = Path.Combine(editorPluginNativeFolder, "win", "x86_64");
-                    native32LibPath = Path.Combine(editorPluginNativeFolder, "win", "x86");
+                    {
+                        native64LibPath = Path.Combine(editorPluginNativeFolder, "win", "x86_64");
+                        native32LibPath = Path.Combine(editorPluginNativeFolder, "win", "x86");
+                    }
                     break;
                 case RuntimePlatform.WindowsPlayer:
-                    native64LibPath = pluginsFolder + "/x86_64";
-                    native32LibPath = pluginsFolder + "/x86";
+                    {
+                        //Seems like windows they are not added to any specific folder, just dropped inside Plugins folder
+                        native64LibPath = pluginsFolder;
+                        native32LibPath = pluginsFolder;
+                    }
                     break;
                 case RuntimePlatform.LinuxEditor:
-                    native64LibPath = Path.Combine(editorPluginNativeFolder, "linux", "x86_64");
-                    native32LibPath = Path.Combine(editorPluginNativeFolder, "linux", "x86");
+                    {
+                        native64LibPath = Path.Combine(editorPluginNativeFolder, "linux", "x86_64");
+                        native32LibPath = Path.Combine(editorPluginNativeFolder, "linux", "x86");
+                    }
                     break;
                 case RuntimePlatform.LinuxPlayer:
-                    //Linux also drop package plugins inside Plugins folder
-                    native64LibPath = pluginsFolder;
-                    native32LibPath = pluginsFolder;
+                    {
+                        //Linux standalone creates subfolders presumably since it allows "universal" types
+                        native64LibPath = Path.Combine(pluginsFolder, "x86_64");
+                        native32LibPath = Path.Combine(pluginsFolder, "x86");
+                    }
                     break;
                 case RuntimePlatform.OSXEditor:
-                    native64LibPath = Path.Combine(editorPluginNativeFolder, "osx", "x86_64");
-                    native32LibPath = Path.Combine(editorPluginNativeFolder, "osx", "x86");
+                    {
+                        native64LibPath = Path.Combine(editorPluginNativeFolder, "osx", "x86_64");
+                        native32LibPath = Path.Combine(editorPluginNativeFolder, "osx", "x86");
 
-                    //In order to get unity to accept the dylib, had to rename it as *.bundle. Set an override name so we try and load that file. Seems to load
-                    //fine.
-                {
-                    string bundlelibName = Path.ChangeExtension(libInstance.DefaultLibraryName, ".bundle");
-                    override64LibName = bundlelibName;
-                    override32LibName = bundlelibName;
-                }
+                        //In order to get unity to accept the dylib, had to rename it as *.bundle. Set an override name so we try and load that file. Seems to load
+                        //fine.
+                        string bundlelibName = Path.ChangeExtension(libInstance.DefaultLibraryName, ".bundle");
+                        override64LibName = bundlelibName;
+                        override32LibName = bundlelibName;
+                    }
                     break;
                 case RuntimePlatform.OSXPlayer:
-                    native64LibPath = pluginsFolder;
-                    native32LibPath = pluginsFolder;
+                    {
+                        native64LibPath = pluginsFolder;
+                        native32LibPath = pluginsFolder;
 
-                    //In order to get unity to accept the dylib, had to rename it as *.bundle. Set an override name so we try and load that file. Seems to load
-                    //fine.
-                {
-                    string bundlelibName = Path.ChangeExtension(libInstance.DefaultLibraryName, ".bundle");
-                    override64LibName = bundlelibName;
-                    override32LibName = bundlelibName;
-                }
+                        //In order to get unity to accept the dylib, had to rename it as *.bundle. Set an override name so we try and load that file. Seems to load
+                        //fine.
+                        string bundlelibName = Path.ChangeExtension(libInstance.DefaultLibraryName, ".bundle");
+                        override64LibName = bundlelibName;
+                        override32LibName = bundlelibName;
+                    }
                     break;
-                //TODO: Add more platforms if you have binaries that can run on it
+                case RuntimePlatform.WSAPlayerARM:
+                case RuntimePlatform.WSAPlayerX86:
+                case RuntimePlatform.WSAPlayerX64:
+                    {
+                        // Needs special treatment, see below.
+                        native64LibPath = "";
+                        native64LibPath = "";
+
+                        override64LibName = "assimp.dll";
+                        override32LibName = "assimp.dll";
+                    }
+                //TODO: Add more platforms if you have binaries that can run on it, examples:
             }
 
             //If both null, then we do not support the platform
-            if (native64LibPath == null && native32LibPath == null)
+            if(native64LibPath is null && native32LibPath is null)
             {
                 Debug.Log(string.Format("Assimp does not support platform: {0}", Application.platform.ToString()));
                 s_assimpAvailable = false;
@@ -144,12 +175,32 @@ namespace Assimp
             libInstance.ThrowOnLoadFailure = false;
 
             //Try and load the native library, if failed we won't get an exception
-            bool success = libInstance.LoadLibrary();
+            bool success;
+            
+            if (Application.platform == RuntimePlatform.WSAPlayerARM ||
+                Application.platform == RuntimePlatform.WSAPlayerX86 ||
+                Application.platform == RuntimePlatform.WSAPlayerX64)
+            {
+                //On UWP, the folder hierarchy is flattened, making the DLL end up next to the executable. This location
+                //is already in the DLL search path, so we don't need an absolute path (we wouldn't know which one to
+                //provide either). You have to set the DLLs to only be included on the appropriate platform in Unity,
+                //to ensure only the right library is included in your build.
+                success = libInstance.LoadLibrary(override64LibName);
+            }
+            else
+            {
+                success = libInstance.LoadLibrary();
+            }
             s_assimpAvailable = success;
             s_triedLoading = true;
 
             //Turn exceptions back on
             libInstance.ThrowOnLoadFailure = true;
+        }
+        
+        private static string GetCallerFilePath([System.Runtime.CompilerServices.CallerFilePath] string filepath = "")
+        {
+            return filepath;
         }
     }
 }
